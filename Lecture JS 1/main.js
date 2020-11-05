4d6f3e4b22dbac6a28bf13a890891e7a8d31e6d7
dmitrolov <dmitrolov@gmail.com>
2020-11-05 22:30:10 +0200

    Lecture js 1

---
+++ Lecture JS 1/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const user = 'John 😀';
+
+
+let age = 25;
+let message = `Hello ${user}!`;
+
+console.log(message);
+
+function Plus (a, b) {
+ return a + b
+}
+let a = 21474836499999999999999999999999999
+a = 9223372036854775807
+console.log(user[0]);
+
+let b = 1;
+let c = b;
+b = 2;
+console.log(c);
+
+let d = { a: 1 };
+
+let admin
+let name;
+admin = name;
+console.log(admin);
+let earthName = 'Земля';
+let user = {
+  name: 'asd',
+  age: 12
+}
+let userName = user.name;
+
+
+let b = 1;
+let c = b;
+b = 2;
+console.log(c);
+
+let d = { 
+  name: 1 
+};
+let e = d;
+d.name = 2;
+console.log(e.name);
+
+let name1 = "Ilya";
+console.log(`hello ${1}`)
+// `hello ${"name1"}` => hello "Ilya"
+console.log(`hello ${"name1"}`)
+// `hello ${name1}` =>   hello Ilya
+console.log(`hello ${name1}`)
+
+const aa = '1a';
+const bb = '2a';
+console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+console.log(1 + - + + - - + + + + + + + - 2) // 1 + - + + - + - 2 // -1 D | 3 O
+
